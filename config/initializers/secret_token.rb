cd77d634f56bf0d01b46ef62b99b47e525928cd6
Kentoku Matsunami <kentoku.matsunami@gmail.com>
2012-05-04 16:02:23 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sadcs::Application.config.secret_token = '420919f1a21ed037031850ac6be449e611a03a7b507ddca5000f1424ac96d2ae9224af20c335335bf98b9efcf756435ee8210dfa9b87bc7704f83b07d5aea66b'
